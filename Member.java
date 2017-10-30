9ac46faeab854ccc048776fe665066debacf0728
Peter Crowther <petercrowther90@hotmail.com>
2017-10-30 22:01:21 +0000

    created member class with array length of one and 2 borrowed books by the member

--- Member.java
+++ Member.java
@@ -1,3 +1,11 @@
 public class Member{
+  int borrowed;
+
+  public static void main(String[] args) {
+    Member[] memberArray = new Member[1];
+    memberArray[0] = new Member();
+    memberArray[0].borrowed = 2;
+  }
+
 
 }
